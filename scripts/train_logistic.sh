56e5e38ac7ca4685c9cdf8739573b43a22e1b040
diex <diex@live.cn>
2025-06-08 13:22:05 +0800

    Update train.py and wandb settings

--- scripts/train_logistic.sh
+++ scripts/train_logistic.sh
@@ -1,2 +1,2 @@
-# Just for test
-uv run train.py --model=logistic --bs=1024 --ep=50 --tblr=1e-5 --save_path=./ckpts
+wandb online
+python train.py --dataset=cifar10 --model=logistic --bs=1024 --ep=50 --tblr=1e-5 --save_path=./ckpts --exp_name=logistic --wandb=1
